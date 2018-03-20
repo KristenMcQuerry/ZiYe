fe0aa112f0a9603935d7e3148bcfbd3b1f753140
KristenMcQuerry <35340835+KristenMcQuerry@users.noreply.github.com>
2018-03-20 13:55:02 -0400

    Assignment 2

---
+++ 693 Spring 2018 Assignment 2.R
@@ -0,0 +1,10 @@
+##install packages
+install.packages("tidyverse")
+install.packages("nycflights13")
+
+## load libraries
+library(tidyverse)
+library(nycflights13)
+
+## dataset for assignment
+flights
